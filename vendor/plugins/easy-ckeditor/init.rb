3a200ddc56f46d984a046822061180d025ba0fe6
Edgar Perez <edgarp313@gmail.com>
2012-02-03 00:23:17 -0700

    Disabling write access for Heroku

--- vendor/plugins/easy-ckeditor/init.rb
+++ vendor/plugins/easy-ckeditor/init.rb
@@ -1,9 +1,9 @@
 # Include hook code here
 require 'ckeditor'
 require 'ckeditor_version'
-require 'ckeditor_file_utils'
+#require 'ckeditor_file_utils'
 
-CkeditorFileUtils.check_and_install
+#CkeditorFileUtils.check_and_install
 
 # make plugin controller available to app
 config.autoload_paths += %W(#{Ckeditor::PLUGIN_CONTROLLER_PATH} #{Ckeditor::PLUGIN_HELPER_PATH})
