8eb95ffdac5a872e722d050cd1b6fda987b7ed45
Edgar Perez <edgarp313@gmail.com>
2012-02-03 01:13:10 -0700

    Changed comment type

--- db/migrate/103_really_update_existing_articles_with_nil_or_empty_post_type.rb
+++ db/migrate/103_really_update_existing_articles_with_nil_or_empty_post_type.rb
@@ -4,18 +4,16 @@ class ReallyUpdateExistingArticlesWithNilOrEmptyPostType < ActiveRecord::Migrati
   end
 
   def self.up
-    =begin
-    say_with_time "Really fix existing articles with no post type." do
-      Content.find(:all,
-                   :conditions => ['type = ?', 'Article']).each do |art|
-        if art.post_type.nil? or art.post_type.empty?
-          say "Fixing '#{art.title}'", 1
-          art.post_type = "read"
-          art.save!
-        end
-      end
-    end
-    =end
+    #say_with_time "Really fix existing articles with no post type." do
+      #Content.find(:all,
+       #            :conditions => ['type = ?', 'Article']).each do |art|
+        #if art.post_type.nil? or art.post_type.empty?
+         # say "Fixing '#{art.title}'", 1
+          #art.post_type = "read"
+          #art.save!
+        #end
+      #end
+    #end
   end
 
   def self.down
